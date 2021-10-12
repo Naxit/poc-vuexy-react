e2a596bd43270cf53ffa278fa25f0140e23be10c
AuroreLeblois <leblois-aurore@hotmail.fr>
2021-10-12 18:01:15 +0200

    4 cards sites pages ok

--- src/views/sites/list/index.js
+++ src/views/sites/list/index.js
@@ -1,9 +1,29 @@
+// ** User List Component
+import Table from './Table.js'
+import { Card, Cols, Pills, Dropdown, CardTitle, CardHeader, Select, Input } from 'reactstrap'
+import PickerMonth from 'react-month-picker-input'
+
 const ListSites = () => {
-  return (
-    <div>
-			Liste des sites
-    </div>
-  )
+	return (
+      <div className='app-user-list'>
+  	  <Card>  <Input
+			id='search-invoice'
+			className='ml-50 w-100'
+			type='text'
+			value={''}
+			onChange={()=>{}}
+		  /><Dropdown/>
+		  <Dropdown/>
+		  </Card>
+  	  <Card>
+  	  <CardHeader>
+   	   <CardTitle tag='h2'>BTelecom - Ouest</CardTitle>
+   	 </CardHeader>
+  	 </Card>
+  	  <Card><Table/></Card>
+  	 <Card><Table/></Card>
+      </div>
+    )
 }
 
 export default ListSites
