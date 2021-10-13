764513e90edac2112e7ccfff42eead3d332a851f
AuroreLeblois <leblois-aurore@hotmail.fr>
2021-10-13 10:00:31 +0200

    card layout

--- src/views/sites/list/index.js
+++ src/views/sites/list/index.js
@@ -1,7 +1,6 @@
 // ** User List Component
 import Table from './Table.js'
 import { Card, Dropdown, CardTitle, CardHeader, Input, Row, Col } from 'reactstrap'
-
 import PickerMonth from 'react-month-picker-input'
 import CardFilters from './CardFilters.js'
 import CardMap from './CardMap.js'
@@ -9,13 +8,20 @@ import CardMap from './CardMap.js'
 const ListSites = () => {
 	return (
 		<div className='app-user-list'>
-			<Row >
-				<Col md='3'><CardFilters/></Col>
-				<Col><CardMap/></Col>
+			<Row>
+				<Col md='2'><h2>Liste des sites</h2></Col>
 			</Row>
 			<Row>
-				<Col md='3'><Table/></Col>
-				<Col><Table/></Col>
+				<Col md='3'>
+					<CardFilters/>
+					<Table/>
+				</Col>
+
+				<Col>
+					<CardMap/>
+					<Table/>
+
+				</Col>
 			</Row>
 		</div>
 	)
